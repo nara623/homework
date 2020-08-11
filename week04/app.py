bfda4d06fb0d242d944f0890fcc5c4f54a56c1ed
nara623 <nara623@gmail.com>
2020-08-12 01:48:54 +0900

    Create app.py

---
+++ week04/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbhomework
+
+
+## HTML 화면 보여주기
+@app.route('/')
+def homework():
+    return render_template('index.html')
+
+
+# 주문하기(POST) API
+@app.route('/order', methods=['POST'])
+def save_order():
+    name_receive = request.form['name_give']
+    count_receive = request.form['count_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+# 여길 채워나가세요!
+    order = {
+        'name': name_receive,
+        'count': count_receive,
+        'address': address_receive,
+        'phone': phone_receive
+    }
+    db.orders.insert_one(order)
+    return jsonify({'result': 'success', 'msg': '주문이 성공적으로 작성되었습니다.'})
+
+
+# 주문 목록보기(Read) API
+@app.route('/order/view', methods=['GET'])
+def view_orders():
+# 여길 채워나가세요!
+    orders = list(db.orders.find({}, {"_id": False}))
+    return jsonify({'result': 'success', 'orders': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
